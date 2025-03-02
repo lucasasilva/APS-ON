5ec09a0754d1f24579c91e91c201f6a71b0cd364
lucasasilva <las.lucasantonio@gmail.com>
2025-03-02 20:14:15 -0300

    Criado enums e alteração da documentação

---
+++ APS-ON/src/main/java/util/Enums.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Enums {
+    public enum tipoCadastro {
+        Aluno, Professor
+    }
+    public enum tipoAtividade{
+        APS, EPS
+    }
+    public enum periodos {
+        primeiro, segundo, terceiro, quarto, quinto, sexto, setimo, oitavo
+    }
+    public enum disponibilidadeDias{
+        segunda, terca, quarta, quinta, sexta
+    }
+}
