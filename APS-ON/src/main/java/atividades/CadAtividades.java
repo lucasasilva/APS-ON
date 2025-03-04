557e1a8f16ad9cae6c6f0a50a67961bbd33e7683
lucasasilva <las.lucasantonio@gmail.com>
2025-03-04 14:43:09 -0300

    Ajustado tipo do map da CadInstituicoesSaude Criado classe para area de atuação

--- APS-ON/src/main/java/atividades/CadAtividades.java
+++ APS-ON/src/main/java/atividades/CadAtividades.java
@@ -2,5 +2,8 @@ package atividades;
 
 import util.Enums;
 public class CadAtividades {
+    private int id;
+    private Enums.tipoAtividade tipoAtividade;
+    private int areaAtuacaoMedica; //depende da cadAreas
 
 }
--- APS-ON/src/main/java/cadastros/CadInstituicoesSaude.java
+++ APS-ON/src/main/java/cadastros/CadInstituicoesSaude.java
@@ -3,5 +3,13 @@ package cadastros;
 import java.util.Map;
 
 public class CadInstituicoesSaude {
-    public Map<Integer, String> cadInstituicoes;
+    private Map<Integer, String> cadInstituicoes;
+
+    public Map<Integer, String> getCadInstituicoes() {
+        return cadInstituicoes;
+    }
+
+    public void setCadInstituicoes(Map<Integer, String> cadInstituicoes) {
+        this.cadInstituicoes = cadInstituicoes;
+    }
 }
