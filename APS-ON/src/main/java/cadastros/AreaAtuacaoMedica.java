57b147009bbcf327c782acfa1e5c114ec4b6ee7b
lucasasilva <las.lucasantonio@gmail.com>
2025-03-04 14:44:30 -0300

    Ajustado tipo do map da CadInstituicoesSaude Criado classe para area de atuação

---
+++ APS-ON/src/main/java/cadastros/AreaAtuacaoMedica.java
@@ -0,0 +1,15 @@
+package cadastros;
+
+import java.util.Map;
+
+public class AreaAtuacaoMedica {
+    private Map<Integer, String> cadAreaAtuacaoMedica;
+
+    public Map<Integer, String> getCadAreaAtuacaoMedica() {
+        return cadAreaAtuacaoMedica;
+    }
+
+    public void setCadAreaAtuacaoMedica(Map<Integer, String> cadAreaAtuacaoMedica) {
+        this.cadAreaAtuacaoMedica = cadAreaAtuacaoMedica;
+    }
+}
