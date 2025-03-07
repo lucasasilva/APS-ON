bffc86ef9268fecb47eaaeed136a28923c38abc8
lucasasilva <las.lucasantonio@gmail.com>
2025-03-06 21:39:43 -0300

    Classe para menus

---
+++ APS-ON/src/main/java/util/Menu.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Menu {
+    public static void menu (){
+        System.out.println("Escolha uma opção");
+        System.out.println("1 - Cadastro de professores         2 - Cadastro de alunos");
+        System.out.println("3 - Cadastro de Instituições         4 - Cadastro de Áreas médicas");
+        System.out.println("4 - Cadastrar APS                   5 - Ver cadastros");
+        System.out.println("6 - Sair");
+
+    }
+    public static void menuRelatorios(){
+        System.out.println("1 - Ver cadastro de professores     2 - Ver cadastro de alunos");
+        System.out.println("3 - Ver cadastro de instituições" + "4 - Ver Cadastro de Áreas");
+        System.out.println("5 - voltar");
+    }
+}
