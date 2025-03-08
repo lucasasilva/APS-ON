cec77626996aee4b3e3d832df09904fc3b56a7ef
lucasasilva <las.lucasantonio@gmail.com>
2025-03-07 22:16:45 -0300

    Area atuação medica na main porque eu fodi a minha branch
    Signed-off-by: lucasasilva <las.lucasantonio@gmail.com>

---
+++ APS-ON/src/main/java/cadastros/AreaAtuacaoMedica.java
@@ -0,0 +1,47 @@
+package cadastros;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AreaAtuacaoMedica {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void criaCadastro (Scanner sc, Map<Integer, String> lista){
+        char vParada;
+        do {
+            AreaAtuacaoMedica object = new AreaAtuacaoMedica();
+            System.out.println("Qual a id da area? ");
+            object.setId(sc.nextInt());
+            sc.nextLine();
+            System.out.println("Qual o nome da area");
+            object.setNome(sc.nextLine());
+            lista.put(object.getId(), object.getNome());
+            System.out.println("Deseja continuar cadastrando areas?");
+            vParada = sc.next().charAt(0);
+        }while (Character.toUpperCase(vParada)=='S');
+        //asdfasdf
+    }
+    //TOME-LHE POLIMORFISMO LEK, DRUIDA ATÉ NO CÓDIGO
+    @Override
+    public String toString() {
+        return this.getId() + " " + this.getNome();
+    }
+}
