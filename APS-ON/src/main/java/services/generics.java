f97033f138048f550eb38db51e61813ddbd194cd
lucasasilva <las.lucasantonio@gmail.com>
2025-04-13 14:44:29 -0300

    Documentado: Do cu mentado: que possui o c* sabor "menta"; Documentando como os métodos funcionam.

--- APS-ON/src/main/java/services/generics.java
+++ APS-ON/src/main/java/services/generics.java
@@ -16,6 +16,20 @@ public class generics {
 
        Dessa forma, podemos usar sempre a mesma query para trazermos TODOS OS CAMPO em um objeto ou
        campos específicos (Em caso de campos específicos, requer um construtor que contenha somente ele)
+
+
+       como funciona?
+       - SessionFactory vai buscar, dentro do hibernate.cfg.xml os dados do nosso banco de dados; IMPORTANTE:
+       Só funciona do jeito que está declarado ali se arquivo for o padrão hibernate.cfg.xml
+       Se for outro nome, precisa ser declarado como new Configuration().configure(nome_arquivo.cfg.xml).buildSessionFactory();
+
+       - session usa o objeto do tipo SessionFactory para abrir uma conexão com o banco. É a variável que usaremos para declarar
+       o que exatamente estaremos realizando dentro do banco (com métodos .createQuery() .persist() .merge() .remove())
+
+       - Transaction, como o nome sugere, usa a conexão aberta para controlar uma transação (select, insert, update, delete) que fora aberta
+       pela session. Commit, rollback, iara iara, a putaria toda.
+
+       Depois tem que fechar todo mundo, senão DREADLOCKS
     */
     public static <T> List<T> selectBanco(String query, Class<T> classeDoObjeto){
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -31,7 +45,11 @@ public class generics {
         return resultList;
     }
 
-
+    /* As diferenças daqui para baixo:
+    Objeto genérico: Não importa a classe, funciona para todas;
+    try - catch: Boa prática para evitar que a transação de errado e a gente sem saber o porquê;
+    Faz rollback automático (=
+    * */
     public static <T> void insertBanco(Object objetoGenerico){
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
