e9a5e0e9bbde52cc6bef8d84be8b45a2eb5dd871
dayane.marques <contato.dayanemarques@outlook.com>
2025-04-16 19:04:54 -0300

    classe instuições saude e mapeamento hibernate

--- APS-ON/src/main/java/br/com/apson/entities/CadInstituicoesSaude.java
+++ APS-ON/src/main/java/br/com/apson/entities/CadInstituicoesSaude.java
@@ -4,16 +4,35 @@ import br.com.apson.util.Enums;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import jakarta.persistence.*;
 
-public class CadInstituicoesSaude extends SuperCadastros {
-    /*
-    * TODO refazer essa tabela conforme o banco. Nao pode ser essa lista*/
-
-    private List<CadInstituicoesSaude> instituicoesSaudes = new ArrayList<>();
-    private Scanner scanner = new Scanner(System.in);
+@Entity
+@Table (name = "cad_instituicoes_saude")
+public class CadInstituicoesSaude {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column (name = "nome")
+    private String nome;
 
     public List<CadInstituicoesSaude> getInstituicoesSaudes() {
         return instituicoesSaudes;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+
+    }
 }
