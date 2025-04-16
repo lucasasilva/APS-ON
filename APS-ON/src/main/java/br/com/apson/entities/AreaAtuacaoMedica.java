1054a5a5591238eb4bc35ff2b5594a5a0bf3ce3f
Gabriel <127374872+gabrielmeloaraujo@users.noreply.github.com>
2025-04-16 19:05:13 -0300

    mapeamento cad_alunos e areaAtuacaoMedica, ajustes na SupoerCadastros

--- APS-ON/src/main/java/br/com/apson/entities/AreaAtuacaoMedica.java
+++ APS-ON/src/main/java/br/com/apson/entities/AreaAtuacaoMedica.java
@@ -1,14 +1,38 @@
 package br.com.apson.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cad_area_atuacao_medica")
 public class AreaAtuacaoMedica {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
     private String nome;
 
-    public int getId() {
+    // Construtor pad
+    public AreaAtuacaoMedica() {
+    }
+
+    // Construtor nome
+    public AreaAtuacaoMedica(String nome) {
+        this.nome = nome;
+    }
+
+    // Construtor completo
+    public AreaAtuacaoMedica(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -19,5 +43,4 @@ public class AreaAtuacaoMedica {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 }
--- APS-ON/src/main/java/br/com/apson/entities/CadAlunos.java
+++ APS-ON/src/main/java/br/com/apson/entities/CadAlunos.java
@@ -1,16 +1,35 @@
 package br.com.apson.entities;
 
-import br.com.apson.util.Enums;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "cad_alunos")
 public class CadAlunos extends SuperCadastros {
 
-
-    private int periodo;
+    @Column(name = "registro_aluno", nullable = false, unique = true)
     private int ra;
 
+    @Column(name = "periodo", nullable = false)
+    private int periodo;
+
+    // Construtor padrão
+    public CadAlunos() {
+    }
+
+    // Construtor com os campo da classe CadAlunos
+    public CadAlunos(int ra, int periodo) {
+        this.ra = ra;
+        this.periodo = periodo;
+    }
+
+    // Construtor com os campos herdado da SuperCadastros
+    public CadAlunos(long id, String telefoneContato, String emailContato, String login, String senha, String nome, int ra, int periodo) {
+        super(id, telefoneContato, emailContato, login, senha, nome);
+        this.ra = ra;
+        this.periodo = periodo;
+    }
+
+    // Getters e Setters
     public int getRa() {
         return ra;
     }
@@ -26,6 +45,4 @@ public class CadAlunos extends SuperCadastros {
     public void setPeriodo(int periodo) {
         this.periodo = periodo;
     }
-
-
 }
