a20c52e8d77b53638376bc0b33371bff0d956b75
lucasasilva <las.lucasantonio@gmail.com>
2025-04-23 22:26:52 -0300

    Testes da main

--- APS-ON/src/main/java/br/com/apson/Main.java
+++ APS-ON/src/main/java/br/com/apson/Main.java
@@ -1,16 +1,20 @@
 package br.com.apson;
 
+import br.com.apson.entities.CadInstituicoesSaude;
 import br.com.apson.entities.SuperCadastros;
 
+import br.com.apson.repository.CadInstituicoesSaudeInterface;
+import br.com.apson.repository.CadInstituicoesSaudeRepImpInterface;
 import br.com.apson.repository.CadProfessoresRepImp;
 import br.com.apson.repository.CadProfessoresRepInterface;
+import br.com.apson.services.CadInstuicoesSaudeService;
 import br.com.apson.util.generics;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       // Scanner sc = new Scanner(System.in);
 
         /* Aqui passamos a query que queremos usar como parâmetro do select genérico.
         * Importante: Se queremos somente alguns campos (e não todos os campos da classe/objeto) temos que passar:
@@ -20,15 +24,27 @@ public class Main {
         * estão vindo os dados
         *
         * ABAIXO DOIS EXEMPLOS DE COMO FAZÊ-LO*/
-        String query = "from SuperCadastros";
+//        String query = "from SuperCadastros";
+//
+//        List<SuperCadastros> pessoas  = generics.selectBanco(query, SuperCadastros.class);
+//
+//        for (SuperCadastros pessoa:pessoas){
+//            System.out.println(pessoa.toString());
+//        }
+//        sc.close();
 
-        List<SuperCadastros> pessoas  = generics.selectBanco(query, SuperCadastros.class);
 
-        for (SuperCadastros pessoa:pessoas){
-            System.out.println(pessoa.toString());
+        CadInstuicoesSaudeService cadInstuicoesSaudeService = new CadInstuicoesSaudeService(new CadInstituicoesSaudeRepImpInterface());
+        //CadInstituicoesSaude novaInstituicao = new CadInstituicoesSaude("Upa Vila A");
+        //cadInstuicoesSaudeService.criarInstituicao(novaInstituicao);
+        List<CadInstituicoesSaude> lista =  cadInstuicoesSaudeService.retornaTodasInstituicoes();
+        CadInstituicoesSaude retorno = cadInstuicoesSaudeService.retornaInstuicaoByID(3);
+        for (CadInstituicoesSaude item : lista){
+            System.out.println(item.getId()+":"+item.getNome());
         }
-        sc.close();
 
+        System.out.println("\nRetorno de um id: \n");
+        System.out.println(retorno.getId()+":"+retorno.getNome());
     }
 }
 
