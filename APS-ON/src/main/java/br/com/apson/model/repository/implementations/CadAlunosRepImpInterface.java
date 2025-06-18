1ffeed6ebdd00d87f191fb8376492ea90cf8003b
lucasasilva <las.lucasantonio@gmail.com>
2025-06-17 23:45:37 -0300

    Não sei porque to tendo que subir essas merda de novo, mas tudo bem...

---
+++ APS-ON/src/main/java/br/com/apson/model/repository/implementations/CadAlunosRepImpInterface.java
@@ -0,0 +1,29 @@
+package br.com.apson.model.repository.implementations;
+
+import br.com.apson.model.entities.CadAlunos;
+import br.com.apson.model.repository.CadAlunosRepInterface;
+import br.com.apson.util.generics;
+
+import java.util.List;
+
+public class CadAlunosRepImpInterface implements CadAlunosRepInterface {
+    @Override
+    public void criarAluno(CadAlunos aluno) {
+        generics.insertBanco(aluno);
+    }
+
+    @Override
+    public void alterarAluno(CadAlunos aluno) {
+        generics.updateBanco(aluno);
+    }
+
+    @Override
+    public List<CadAlunos> retornaAlunos() {
+        return generics.selectBanco("from CadAlunos", CadAlunos.class);
+    }
+
+    @Override
+    public CadAlunos retornaAlunoPorID(int id) {
+        return generics.selectBancoByID("from CadAlunos", CadAlunos.class, id);
+    }
+}
---
+++ APS-ON/src/main/java/br/com/apson/model/repository/implementations/CadAtividadesImplementation.java
@@ -0,0 +1,29 @@
+package br.com.apson.model.repository.implementations;
+
+import br.com.apson.model.entities.CadAtividades;
+import br.com.apson.model.repository.CadAtividadesInterface;
+import br.com.apson.util.generics;
+
+import java.util.List;
+
+public class CadAtividadesImplementation implements CadAtividadesInterface {
+    @Override
+    public int criarAtividade(CadAtividades obj) {
+        return generics.insertBancoRetornaID(obj);
+    }
+
+    @Override
+    public void alterarAtividade(CadAtividades obj) {
+        generics.updateBanco(obj);
+    }
+
+    @Override
+    public CadAtividades retornaAtividadeById(int id) {
+        return generics.selectBancoByID("from CadAtividades", CadAtividades.class, id);
+    }
+
+    @Override
+    public List<CadAtividades> retornaTodasAsAtividade() {
+        return generics.selectBanco("from CadAtividades", CadAtividades.class);
+    }
+}
