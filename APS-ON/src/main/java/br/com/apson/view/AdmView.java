a49631559fae0fe5a63ef5d4fa4d9c910da32e42
lucasasilva <las.lucasantonio@gmail.com>
2025-06-22 15:31:23 -0300

    adm tem acesso a exclusão de informações, coisa que ninguém mais tem.

--- APS-ON/src/main/java/br/com/apson/view/AdmView.java
+++ APS-ON/src/main/java/br/com/apson/view/AdmView.java
@@ -3,10 +3,19 @@ package br.com.apson.view;
 import javax.swing.*;
 
 public class AdmView {
-    public static void admView(){
-        JFrame frame = new JFrame("APS-ON - admin console");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public void admView(){
+        FramePadrao framePadrao = new FramePadrao();
+        JMenuItem excluirArea = new JMenuItem("Excluir Cadastro");
+        JMenuItem excluirInstituicao = new JMenuItem("Excluir Cadastro");
+        JMenuItem excluirProfessor = new JMenuItem("Excluir Cadastro");
+        JMenuItem excluirAluno = new JMenuItem("Excluir Cadastro");
+        JMenuItem excluirAtividade  = new JMenuItem("Excluir Cadastro");
 
-        frame.setVisible(true);
+
+        framePadrao.areaAtuacao.add(excluirArea);
+        framePadrao.instituicaoAtuacao.add(excluirInstituicao);
+        framePadrao.professores.add(excluirProfessor);
+        framePadrao.alunos.add(excluirAluno);
+        framePadrao.atividades.add(excluirAtividade);
     }
 }
