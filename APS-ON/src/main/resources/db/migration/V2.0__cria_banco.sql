0a31effae46b91d3e9bb36162dd0ceef6fd4e11a
lucasasilva <las.lucasantonio@gmail.com>
2025-06-24 22:35:57 -0300

    Ajustado script do banco, tabelas e campos que estavam faltando. Inseridas regras de chaves estrangeiras com cascade

--- APS-ON/src/main/resources/db/migration/V2.0__cria_banco.sql
+++ APS-ON/src/main/resources/db/migration/V2.0__cria_banco.sql
@@ -14,7 +14,7 @@ CREATE TABLE cad_area_atuacao_medica (
                                          CONSTRAINT pk_cad_area_atuacao_medica PRIMARY KEY (id)
 );
 
-CREATE TABLE cad_instituicao_saude (
+CREATE TABLE cad_instituicoes_saude (
                                        id SERIAL,
                                        nome VARCHAR(30),
                                        CONSTRAINT pk_cad_instituicao_saude PRIMARY KEY (id)
@@ -25,20 +25,24 @@ CREATE TABLE cad_professores (
                                  id INTEGER,
                                  instituicao_trabalho INTEGER,
                                  CONSTRAINT pk_cad_professores PRIMARY KEY (id),
-                                 CONSTRAINT fk_instituicao_trabalho FOREIGN KEY (instituicao_trabalho) REFERENCES cad_instituicao_saude(id)
+                                 CONSTRAINT fk_cad_prof_pessoa FOREIGN KEY (ID) REFERENCES CAD_PESSOAS(ID) on DELETE CASCADE,
+                                 CONSTRAINT fk_instituicao_trabalho FOREIGN KEY (instituicao_trabalho) REFERENCES cad_instituicoes_saude(id)
 );
 
 CREATE TABLE cad_prof_dias_disp (
                                     id SERIAL,
                                     dia_semana INTEGER,
                                     cod_professor INTEGER,
-                                    CONSTRAINT pk_cad_prof_dias_disp PRIMARY KEY (id)
+                                    CONSTRAINT pk_cad_prof_dias_disp PRIMARY KEY (id),
+                                    CONSTRAINT FK_CAD_PROF_DIAS_DISP FOREIGN KEY (COD_PROFESSOR) REFERENCES CAD_PROFESSORES(ID) ON DELETE CASCADE
 );
 
 CREATE TABLE cad_alunos (
                             id INTEGER,
                             registro_aluno INTEGER,
-                            CONSTRAINT pk_cad_alunos PRIMARY KEY (id)
+                            periodo integer,
+                            CONSTRAINT pk_cad_alunos PRIMARY KEY (id),
+                            CONSTRAINT FK_ALUNO_PESSOA FOREIGN KEY (ID) REFERENCES CAD_PESSOAS(ID) ON DELETE CASCADE
 );
 
 CREATE TABLE cad_atividades (
@@ -55,21 +59,33 @@ CREATE TABLE cad_atividades (
                                 CONSTRAINT fk_area_atuacao_medica_atividades FOREIGN KEY (area_atuacao_medica) REFERENCES cad_area_atuacao_medica (id)
 );
 
+create table cad_atividades_grupos(
+id SERIAL,
+cod_atividade integer,
+cod_grupo integer,
+CONSTRAINT PK_CAD_ATIVIDADES_GRUPOS PRIMARY KEY (ID),
+constraint fk_ativ_grupo_atividades FOREIGN KEY (COD_ATIVIDADE) REFERENCES CAD_ATIVIDADES(ID) ON DELETE CASCADE
+);
+
 CREATE TABLE cad_ativ_grupo_alunos (
                                        id SERIAL,
+                                       COD_GRUPO INTEGER,
                                        cod_atividade INTEGER,
                                        codigo_aluno INTEGER,
+                                       QTD_VAGAS INTEGER,
+                                       QTD_VAGAS_SOBRANDO INTEGER,
                                        CONSTRAINT pk_cad_ativ_grupo_alunos PRIMARY KEY (id),
-                                       CONSTRAINT fk_cod_atividade_grupos FOREIGN KEY (cod_atividade) REFERENCES cad_atividades (id),
+                                       CONSTRAINT fk_cod_atividade_grupos FOREIGN KEY (cod_atividade) REFERENCES cad_atividades (id) ON DELETE CASCADE,
+                                       CONSTRAINT FK_GRUPO_ALUNO FOREIGN KEY(COD_GRUPO) REFERENCES CAD_ATIVIDADES_GRUPOS (ID) ON DELETE CASCADE,
                                        CONSTRAINT fk_codigo_aluno_grupo_alunos FOREIGN KEY (codigo_aluno) REFERENCES cad_alunos (id)
 );
 
 CREATE TABLE cad_ativ_horarios (
                                    id SERIAL,
                                    cod_atividade INTEGER,
-                                   horarios TIMESTAMP,
+                                   horarios TIME,
                                    CONSTRAINT pk_cad_ativ_horarios PRIMARY KEY (id),
-                                   CONSTRAINT fk_cod_atividade_horarios FOREIGN KEY (cod_atividade) REFERENCES cad_atividades (id)
+                                   CONSTRAINT fk_cod_atividade_horarios FOREIGN KEY (cod_atividade) REFERENCES cad_atividades (id) ON DELETE CASCADE
 );
 
 CREATE TABLE cad_ativ_periodos (
--- APS-ON/src/main/resources/db/migration/V3.0__alteracao_de_estrutura.sql
+++ APS-ON/src/main/resources/db/migration/V3.0__alteracao_de_estrutura.sql
@@ -1,14 +1,22 @@
 alter table cad_atividades
 add periodo varchar(10);
-create table cad_atividades_grupos(
-id SERIAL,
-cod_atividade integer,
-cod_grupo integer,
-constraint fk_ativ_grupo_atividades FOREIGN KEY (COD_ATIVIDADE) REFERENCES CAD_ATIVIDADES(ID)
-);
 
 DROP TABLE CAD_ATIV_PERIODOS;
 
-alter table cad_pessoas add tipo varchar(3);
+alter table cad_pessoas add tipo varchar(1);
 
 insert into cad_pessoas (nome, login, senha, tipo) values ('admin', 'admin','admin','S');
+
+create or replace FUNCTION BLOCK_DELETE_ADMIN()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF old.id = 1 THEN
+	RAISE EXCEPtION 'Não é permitido excluir usuário admin seu macaco prego troncho';
+	end if;
+	RETURN old;
+END;
+$$ LANGUAGE plpgsql;
+
+create trigger trg_block_delete_admin
+BEFORE delete on cad_pessoas
+for each row execute function BLOCK_DELETE_ADMIN();
